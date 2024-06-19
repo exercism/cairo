b3f0b97d3ee5ba32a51f059a05ae9e1970775775
Nenad <nenad.misic@nethermind.io>
2024-06-19 20:32:08 +0200

    remove brackets from format script

--- bin/format_exercises.sh
+++ bin/format_exercises.sh
@@ -11,7 +11,6 @@ format_exercises() {
     exercises_path="$repo/exercises/$1"
     source_file_name="$2"
     for exercise_dir in "$exercises_path"/*; do
-        (
         cd "$exercise_dir"
         config_file="$exercise_dir/.meta/config.json"
         if jq --exit-status '.custom?."allowed-to-not-compile"?' "$config_file"; then
@@ -35,9 +34,9 @@ format_exercises() {
             cp "$tmp_file" "$file_name"
         fi
         rm $tmp_file
-    )
     done
 }
+
 # https://github.com/exercism/docs/blob/main/anatomy/tracks/concept-exercises.md#file-exemplar-implementation
 # format_exercises "concept" "exemplar"
 # https://github.com/exercism/docs/blob/main/anatomy/tracks/practice-exercises.md#file-example-implementation
