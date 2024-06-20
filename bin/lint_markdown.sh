d3b1b33f494c3fb9d8862fbf2a6cf1b8c2519868
Nenad Misić <nenad.misic@nethermind.io>
2024-06-20 09:32:14 +0200

    Add Lucians luscious lasagna exercise (#14)
    * add spellcheck
    
    * format test.yml
    
    * format md files with prettier
    
    * fix spellcheck issues in sh files
    
    * enable passing a single exercise to verify-exercises + add exit 1 on cd
    
    * refactor shell scripts
    
    * use markdownlint-cli (instead of v2) + fix arm. num md file
    
    * Add Lucians Luscious Lasagna exercise

--- bin/lint_markdown.sh
+++ bin/lint_markdown.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-markdownlint_cli_args="$1"
-
 npx markdownlint-cli2 \
-    $markdownlint_cli_args \
     docs/*.md \
     concepts/**/*.md \
     exercises/**/*.md
