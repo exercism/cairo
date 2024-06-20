2822a51c120b073fbfffe9eec2cfdcbb6bae109b
Nenad <nenad.misic@nethermind.io>
2024-06-20 08:13:49 +0200

    fix spellcheck issues in sh files

--- bin/lint_markdown.sh
+++ bin/lint_markdown.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 markdownlint_cli_args="$1"
 
 npx markdownlint-cli2 \
-    $markdownlint_cli_args \
+   "$markdownlint_cli_args" \
     docs/*.md \
     concepts/**/*.md \
     exercises/**/*.md
