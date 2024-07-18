6c83a8506faef55715833d4c6fb1d8d887d9ffc5
Nenad <nenad.misic@nethermind.io>
2024-07-18 12:25:54 +0200

    Remote test.sh and t.sh

--- t.sh
+++
@@ -1,6 +0,0 @@
-dir="./exercises/*/*"
-for exercise_dir in $dir; do
-    echo "updating $exercise_dir"
-    slug=$(basename $exercise_dir)
-    sed -i "s/%{kebab_slug}/$slug/g" "$exercise_dir/.meta/config.json"
-done
