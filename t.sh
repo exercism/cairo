e667d89105156e878ce68dafde5deb3115edb43a
Nenad <nenad.misic@nethermind.io>
2024-07-18 12:25:14 +0200

    Fix all test name templates in exercises

--- t.sh
+++ t.sh
@@ -2,8 +2,5 @@ dir="./exercises/*/*"
 for exercise_dir in $dir; do
     echo "updating $exercise_dir"
     slug=$(basename $exercise_dir)
-    mkdir -p "$exercise_dir/tests"
-    mv "$exercise_dir/src/tests.cairo" "$exercise_dir/tests/$slug.cairo"
-    sed -i '/#\[cfg(test)\]/,$d' "$exercise_dir/src/lib.cairo"
-    sed -i '/#\[cfg(test)\]/,$d' "$exercise_dir/.meta/example.cairo"
+    sed -i "s/%{kebab_slug}/$slug/g" "$exercise_dir/.meta/config.json"
 done
