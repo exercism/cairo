e65c1a2f65b1affb2d2f275108dd6f6fc2145e89
Nenad <xinef.it@gmail.com>
2024-09-26 14:35:27 +0200

    Remove sentence-per-line rule + pass lint cli args to underlying command

--- bin/lint_markdown.sh
+++ bin/lint_markdown.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-args=()
-
-[[ $1 ]] && args+=("$@")
-
-npx markdownlint-cli2 --config .markdownlint-cli2.yaml "$args[@]"
+npx markdownlint-cli2 --config .markdownlint-cli2.yaml "$@"
