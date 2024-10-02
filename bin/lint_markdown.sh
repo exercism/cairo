b8ef2afbe11487f8a9dd74fe329c3efeaa38d257
Nenad <xinef.it@gmail.com>
2024-10-02 10:47:56 +0200

    Include prettier

--- bin/lint_markdown.sh
+++ bin/lint_markdown.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-npx markdownlint-cli2 --config .markdownlint-cli2.yaml "$@"
+npx prettier --write "concepts/**/*.md" "exercises/concept/**/*.md" "exercises/practice/*/.docs/{hints.md,instructions.append.md}"
+npx markdownlint-cli2 --fix "concepts/**/*.md" "exercises/concept/**/*.md" "exercises/practice/*/.docs/{hints.md,instructions.append.md}"
